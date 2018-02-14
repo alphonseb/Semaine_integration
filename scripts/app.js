b91d943e35049b8702f5b81595f8a3602e37bc60
alphonseb <alphonse.bouy@gmail.com>
2018-02-14 18:06:11 +0100

    Avancement homepage
    Slider + dessous

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,87 @@
 let leftArrow = document.querySelector('.slider-arrow-left')
 let rightArrow = document.querySelector('.slider-arrow-right')
+let slider = document.querySelector('.slider-content')
 
-let img = document.querySelectorAll('.slider-content img')
+let img = document.querySelectorAll('.slider-content div')
+let sliderIndex = document.querySelectorAll('.slider-index div')
+let index = 0
+leftArrow.style.display = 'none'
+sliderIndex[0].classList.add('pink-background')
+
+let sliderTimer = setInterval(
+    function(){ slidePerm()},
+    5000
+  )
+
+function slidePerm(){
+  let indexVisible = 0
+
+  for (let i = 0; i < img.length; i++) {
+    if (img[i].classList.contains('is-visible')) {
+      indexVisible = i
+      }
+    }
+  if (index < img.length-1) {
+    let val = 100*(indexVisible+1)
+    for (let i = 0; i < img.length; i++) {
+      img[i].style.transform = `translateX(-${val}%)`
+      }
 
 
-leftArrow.addEventListener(
+    img[indexVisible].classList.remove('is-visible')
+    sliderIndex[indexVisible].classList.remove('pink-background')
+    img[indexVisible+1].classList.add('is-visible')
+    sliderIndex[indexVisible+1].classList.add('pink-background')
+  }
+  else {
+    let currentTranslateValue = -100*(indexVisible)
+    let newTranslateValue = currentTranslateValue + 100
+    for (let i = 0; i < img.length; i++) {
+      img[i].style.transform = `translateX(${newTranslateValue}%)`
+      }
+
+    img[indexVisible].classList.remove('is-visible')
+    sliderIndex[indexVisible].classList.remove('pink-background')
+    img[indexVisible-1].classList.add('is-visible')
+    sliderIndex[indexVisible-1].classList.add('pink-background')
+  }
+  index++
+  if (index === 6) {
+    index = 0
+  }
+  if (index === 0) {
+    leftArrow.style.display = 'none'
+  }
+  else if (index === 3) {
+    rightArrow.style.display = 'none'
+  }
+  else {
+    leftArrow.style.display = 'block'
+    rightArrow.style.display = 'block'
+  }
+}
+
+
+
+// slider.addEventListener(
+//   'mouseenter',
+//   function(){
+//     sliderStop()
+//   },
+//   false
+// )
+//
+// slider.addEventListener(
+//   'mouseleave',
+//   function(){
+//     let sliderTimer = setInterval(
+//         function(){ slidePerm()},
+//         5000
+//       )
+//   }
+// )
+
+rightArrow.addEventListener(
   'click',
   function slideLeft(){
     let indexVisible = 0
@@ -18,17 +95,27 @@ leftArrow.addEventListener(
       for (let i = 0; i < img.length; i++) {
         img[i].style.transform = `translateX(-${val}%)`
       }
-  
+
 
         img[indexVisible].classList.remove('is-visible')
+        sliderIndex[indexVisible].classList.remove('pink-background')
         img[indexVisible+1].classList.add('is-visible')
-
+        sliderIndex[indexVisible+1].classList.add('pink-background')
+        index = indexVisible+1
+        if (index === 3) {
+          rightArrow.style.display = 'none'
+        }
+        else {
+          leftArrow.style.display = 'block'
+          rightArrow.style.display = 'block'
+        }
     }
+
   },
   false
 )
 
-rightArrow.addEventListener(
+leftArrow.addEventListener(
   'click',
   function slideLeft(){
     let indexVisible = 0
@@ -45,12 +132,49 @@ rightArrow.addEventListener(
       }
 
 
-        img[indexVisible].classList.remove('is-visible')
-        img[indexVisible-1].classList.add('is-visible')
-
+      img[indexVisible].classList.remove('is-visible')
+      sliderIndex[indexVisible].classList.remove('pink-background')
+      img[indexVisible-1].classList.add('is-visible')
+      sliderIndex[indexVisible-1].classList.add('pink-background')
+      index = indexVisible-1
+      if (index === 0) {
+        leftArrow.style.display = 'none'
+      }
+      else {
+        leftArrow.style.display = 'block'
+        rightArrow.style.display = 'block'
+      }
     }
-
 
   },
   false
 )
+
+function sliderStop(){
+  clearInterval(sliderTimer)
+}
+
+// for (let k = 0; k < sliderIndex.length; k++) {
+//   sliderIndex[k].addEventListener(
+//     'click',
+//     function moveTo(){
+//       let indexVisible = 0
+//       for (let j = 0; j < img.length; j++) {
+//         if (img[j].classList.contains('is-visible')) {
+//           indexVisible = j
+//         }
+//       }
+//       let newTranslateValue = -k*100
+//
+//       for (let j = 0; j < img.length; j++) {
+//         img[j].style.transform = `translateX(${newTranslateValue}%)`
+//       }
+//       img[indexVisible].classList.remove('is-visible')
+//       sliderIndex[indexVisible].classList.remove('pink-background')
+//       img[k].classList.add('is-visible')
+//       sliderIndex[k].classList.add('pink-background')
+//       index = k
+//     },
+//     false
+//   )
+// }
---
+++ scripts/product.js
@@ -0,0 +1,38 @@
+let titreOnglet = document.querySelectorAll('.homepage-products h2')
+let productRow = document.querySelectorAll('.homepage-products-row')
+let timerIndex = 0
+
+setInterval(
+  function(){
+    for (var j = 0; j < titreOnglet.length; j++) {
+      titreOnglet[j].classList.remove('selected')
+      productRow[j].classList.remove('show')
+    }
+    titreOnglet[timerIndex].classList.toggle('selected')
+    productRow[timerIndex].classList.toggle('show')
+    timerIndex++
+    if (timerIndex === 3) {
+      timerIndex = 0
+    }
+  },
+  5000
+)
+
+for (let i = 0; i < titreOnglet.length; i++) {
+  titreOnglet[i].addEventListener(
+    'click',
+    function(){
+      for (var j = 0; j < titreOnglet.length; j++) {
+        titreOnglet[j].classList.remove('selected')
+        productRow[j].classList.remove('show')
+      }
+      titreOnglet[i].classList.toggle('selected')
+      productRow[i].classList.toggle('show')
+      timerIndex = i+1
+      if (timerIndex === 3) {
+        timerIndex = 0
+      }
+    },
+    false
+  )
+}
