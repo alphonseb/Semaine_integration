52df2d20809707284280c4d3cd652faace2befb5
alphonseb <alphonse.bouy@gmail.com>
2018-02-15 17:40:06 +0100

    update script footer

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,11 +1,15 @@
 let prevDiv = document.querySelector('.homepage-products-row')
 let prevDivHeight = prevDiv.clientHeight
 let margin = prevDivHeight + 60
+let width = window.width()
 
 let reassurance = document.querySelector('#firstPage')
 reassurance.style.marginTop = `${margin}px`
 
 window.addEventListener('resize', function () {
     "use strict";
-    window.location.reload();
+    if (window.width()!==width) {
+      window.location.reload();
+    }
+
 });
