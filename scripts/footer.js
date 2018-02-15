435f32d4a8b7b3852f494cb436d4eeb5bcd9f100
alphonseb <alphonse.bouy@gmail.com>
2018-02-15 17:44:38 +0100

    same

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,14 +1,14 @@
 let prevDiv = document.querySelector('.homepage-products-row')
 let prevDivHeight = prevDiv.clientHeight
 let margin = prevDivHeight + 60
-let width = window.width()
+let width = window.innerWidth
 
 let reassurance = document.querySelector('#firstPage')
 reassurance.style.marginTop = `${margin}px`
 
 window.addEventListener('resize', function () {
     "use strict";
-    if (window.width()!==width) {
+    if (window.innerWidth!==width) {
       window.location.reload();
     }
 
