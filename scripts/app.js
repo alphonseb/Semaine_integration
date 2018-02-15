e42897a9fd060a8687ee40174bda373e46eee3cf
alphonseb <alphonse.bouy@gmail.com>
2018-02-15 23:24:28 +0100

    responsive slider

--- scripts/app.js
+++ scripts/app.js
@@ -55,126 +55,128 @@ function slidePerm(){
   else if (index === 3) {
     rightArrow.style.display = 'none'
   }
-  else {
+  else if(leftArrow.style.display !== 'none' && rightArrow.style.display !== 'none'){
     leftArrow.style.display = 'block'
     rightArrow.style.display = 'block'
   }
 }
 
 
-
-// slider.addEventListener(
-//   'mouseenter',
-//   function(){
-//     sliderStop()
-//   },
-//   false
-// )
-//
-// slider.addEventListener(
-//   'mouseleave',
-//   function(){
-//     let sliderTimer = setInterval(
-//         function(){ slidePerm()},
-//         5000
-//       )
-//   }
-// )
-
 rightArrow.addEventListener(
   'click',
-  function slideLeft(){
-    let indexVisible = 0
-    for (let i = 0; i < img.length; i++) {
-      if (img[i].classList.contains('is-visible')) {
-        indexVisible = i
-      }
-    }
-    if (indexVisible!==img.length-1) {
-      let val = 100*(indexVisible+1)
-      for (let i = 0; i < img.length; i++) {
-        img[i].style.transform = `translateX(-${val}%)`
-      }
-
-
-        img[indexVisible].classList.remove('is-visible')
-        sliderIndex[indexVisible].classList.remove('pink-background')
-        img[indexVisible+1].classList.add('is-visible')
-        sliderIndex[indexVisible+1].classList.add('pink-background')
-        index = indexVisible+1
-        if (index === 3) {
-          rightArrow.style.display = 'none'
-        }
-        else {
-          leftArrow.style.display = 'block'
-          rightArrow.style.display = 'block'
-        }
-    }
-
+  function(){
+    slideRight()
   },
   false
 )
 
 leftArrow.addEventListener(
   'click',
-  function slideLeft(){
-    let indexVisible = 0
-    for (let i = 0; i < img.length; i++) {
-      if (img[i].classList.contains('is-visible')) {
-        indexVisible = i
-      }
-    }
-    if (indexVisible!==0) {
-      let currentTranslateValue = -100*(indexVisible)
-      let newTranslateValue = currentTranslateValue + 100
-      for (let i = 0; i < img.length; i++) {
-        img[i].style.transform = `translateX(${newTranslateValue}%)`
-      }
-
-
-      img[indexVisible].classList.remove('is-visible')
-      sliderIndex[indexVisible].classList.remove('pink-background')
-      img[indexVisible-1].classList.add('is-visible')
-      sliderIndex[indexVisible-1].classList.add('pink-background')
-      index = indexVisible-1
-      if (index === 0) {
-        leftArrow.style.display = 'none'
-      }
-      else {
-        leftArrow.style.display = 'block'
-        rightArrow.style.display = 'block'
-      }
-    }
-
+  function(){
+    slideLeft()
   },
   false
 )
 
-function sliderStop(){
-  clearInterval(sliderTimer)
+
+
+slider.addEventListener('touchstart', handleTouchStart, false);
+slider.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function handleTouchStart(evt) {
+    xDown = evt.touches[0].clientX;
+    yDown = evt.touches[0].clientY;
+};
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            slideRight()/* left swipe */
+        } else {
+            slideLeft()/* right swipe */
+        }
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */
+        } else {
+            /* down swipe */
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
+
+function slideRight(){
+  let indexVisible = 0
+  for (let i = 0; i < img.length; i++) {
+    if (img[i].classList.contains('is-visible')) {
+      indexVisible = i
+    }
+  }
+  if (indexVisible!==img.length-1) {
+    let val = 100*(indexVisible+1)
+    for (let i = 0; i < img.length; i++) {
+      img[i].style.transform = `translateX(-${val}%)`
+    }
+
+
+      img[indexVisible].classList.remove('is-visible')
+      sliderIndex[indexVisible].classList.remove('pink-background')
+      img[indexVisible+1].classList.add('is-visible')
+      sliderIndex[indexVisible+1].classList.add('pink-background')
+      index = indexVisible+1
+      if (index === 3) {
+        rightArrow.style.display = 'none'
+      }
+      else if(leftArrow.style.display !== 'none' && rightArrow.style.display !== 'none'){
+        leftArrow.style.display = 'block'
+        rightArrow.style.display = 'block'
+      }
+  }
+
 }
 
-// for (let k = 0; k < sliderIndex.length; k++) {
-//   sliderIndex[k].addEventListener(
-//     'click',
-//     function moveTo(){
-//       let indexVisible = 0
-//       for (let j = 0; j < img.length; j++) {
-//         if (img[j].classList.contains('is-visible')) {
-//           indexVisible = j
-//         }
-//       }
-//       let newTranslateValue = -k*100
-//
-//       for (let j = 0; j < img.length; j++) {
-//         img[j].style.transform = `translateX(${newTranslateValue}%)`
-//       }
-//       img[indexVisible].classList.remove('is-visible')
-//       sliderIndex[indexVisible].classList.remove('pink-background')
-//       img[k].classList.add('is-visible')
-//       sliderIndex[k].classList.add('pink-background')
-//       index = k
-//     },
-//     false
-//   )
-// }
+function slideLeft(){
+  let indexVisible = 0
+  for (let i = 0; i < img.length; i++) {
+    if (img[i].classList.contains('is-visible')) {
+      indexVisible = i
+    }
+  }
+  if (indexVisible!==0) {
+    let currentTranslateValue = -100*(indexVisible)
+    let newTranslateValue = currentTranslateValue + 100
+    for (let i = 0; i < img.length; i++) {
+      img[i].style.transform = `translateX(${newTranslateValue}%)`
+    }
+
+
+    img[indexVisible].classList.remove('is-visible')
+    sliderIndex[indexVisible].classList.remove('pink-background')
+    img[indexVisible-1].classList.add('is-visible')
+    sliderIndex[indexVisible-1].classList.add('pink-background')
+    index = indexVisible-1
+    if (index === 0) {
+      leftArrow.style.display = 'none'
+    }
+    else if(leftArrow.style.display !== 'none' && rightArrow.style.display !== 'none') {
+      leftArrow.style.display = 'block'
+      rightArrow.style.display = 'block'
+    }
+  }
+
+}
